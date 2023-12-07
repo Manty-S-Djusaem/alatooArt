261d429f1a4a1d7ea36611b5f7702fa38277e297
Manty-S-Djusaem <104922578+Manty-S-Djusaem@users.noreply.github.com>
2023-12-07 23:25:42 +0600

    cartFixed

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,42 +1,56 @@
-import React, { useContext } from 'react';
 import { ContextBox } from '../../App';
+import React, { useContext, useState, useEffect } from 'react';
 import module from './Cart.module.scss';
 
 const Product = ({ item, index }) => {
   const [box, setBox] = useContext(ContextBox);
+  
+};
 
-  const handleQuantityChange = (index, operation) => {
-    const newBox = [...box];
-    const product = newBox[index];
+const Cart = () => {
+  const [box, setBox] = useContext(ContextBox); 
+  const [totalItems, setTotalItems] = useState(0);
+  const [totalPrice, setTotalPrice] = useState(0);
 
-    if (operation === 'add') {
-      product.quantity += 1;
-      if (typeof product.price === 'number' && !isNaN(product.price)) {
-        product.totalPrice = product.quantity * product.price;
-      } else {
-        console.log('Ошибка: неверное значение цены товара');
-        product.totalPrice = 0; // Установка общей стоимости в 0
-      }
-    } else if (operation === 'subtract' && product.quantity > 1) {
-      product.quantity -= 1;
-      if (typeof product.price === 'number' && !isNaN(product.price)) {
-        product.totalPrice = product.quantity * product.price;
-      } else {
-        console.log('Ошибка: неверное значение цены товара');
-        product.totalPrice = 0; // Установка общей стоимости в 0
-      }
+  useEffect(() => {
+    let itemsCount = 0;
+    let totalPrice = 0;
+
+    for (let i = 0; i < box.length; i++) {
+        const count = box[i].count || 1;
+        itemsCount += count;
+        totalPrice += Number(box[i].price) * count;
+        console.log( totalPrice);
     }
 
-    setBox(newBox);
-  };
+    setTotalItems(itemsCount);
+    setTotalPrice(totalPrice);
+}, [box]);
 
-  const removeItem = (index) => {
-    const newBox = [...box];
-    newBox.splice(index, 1);
-    setBox(newBox);
-  };
+const handleIncrease = (index) => {
+  const updatedBox = [...box];
+  updatedBox[index].count = (updatedBox[index].count || 1) + 1;
+  setBox(updatedBox);
+};
 
-  return (
+const handleDecrease = (index) => {
+  const updatedBox = [...box];
+
+  if (updatedBox[index].count > 1) {
+      updatedBox[index].count -= 1;
+      setBox(updatedBox);
+  }
+};
+
+const removeItem = (index) => {
+  const newBox = [...box];
+  newBox.splice(index, 1);
+  setBox(newBox);
+};
+
+  // const totalCost = box.reduce((total, item) => total + item.totalPrice, 0);
+
+  const allProducts = box.map((item, index) => (
     <div className={module.maincards}>
       <div className={module.photo}>
         <div className={module.line2}>
@@ -47,18 +61,15 @@ const Product = ({ item, index }) => {
         <div className={module.name}>
           <h3>{item.title}</h3>
         </div>
-        <div className={module.price}>{item.price}som</div>
+        <div className={module.price}>{item.price * (item.count || 1)}som</div>
         <div className={module.pr}>
-          <button
-            className={module.minus}
-            onClick={() => handleQuantityChange(index, 'subtract')}
-          >
+          <button className={module.minus} onClick={() => handleDecrease(index)}>
             -
           </button>
-          <span className={module.quantity}>{item.quantity}</span>
+          <input  className={module.quantity}  value={item.count || 1} readOnly />
           <button
             className={module.plus}
-            onClick={() => handleQuantityChange(index, 'add')}
+            onClick={() => handleIncrease(index)}
           >
             +
           </button>
@@ -69,22 +80,13 @@ const Product = ({ item, index }) => {
         <div className={module.line}></div>
       </div>
     </div>
-  );
-};
-
-const Cart = () => {
-  const [box] = useContext(ContextBox);
-
-  const totalCost = box.reduce((total, item) => total + item.totalPrice, 0);
-
-  const allProducts = box.map((item, index) => (
-    <Product key={index} item={item} index={index} />
   ));
 
   return (
     <div className={module.container}>
       <h2 className={module.text1}>Корзина</h2>
-      <span className={module.numItems}>Количество товаров: {box.length}</span>
+      <span className={module.numItems}>Количество товаров: {totalItems}</span>
+      <span className={module.numItems}>Итого: {totalPrice}</span>
       <div className={module.products}>{allProducts}</div>
     </div>
   );
